e925d55af22636c672d5ab2781d1d1bc8d3ebbf0
Fabio9494 <fabio.casoria94@gmail.com>
2024-03-21 16:32:06 +0100

    created dashboard

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-    ];
+        'surname',
+        'date_of_birth'
+        ];
 
     /**
      * The attributes that should be hidden for serialization.
