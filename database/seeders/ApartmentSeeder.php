7e8f18c7aa617f6c8090d41e84bb21f9549068dc
asafcastigliano <asafcastigliano@gmail.com>
2024-03-21 17:15:52 +0100

    scritti seeder e migration

--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Apartment;
+
+use Faker\Factory as Faker;
 
 class ApartmentSeeder extends Seeder
 {
@@ -14,6 +17,23 @@ class ApartmentSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create();
+
+        for($i = 0; $i < 5; $i++) {
+            Apartment::create([
+                'title' => $faker->sentence,
+                'city' => $faker->city,
+                'zip_code' => $faker->postcode,
+                'address' => $faker->streetAddress,
+                'rooms' => $faker->numberBetween(1, 5),
+                'bathrooms' => $faker->numberBetween(1, 3),
+                'beds' => $faker->numberBetween(1, 5),
+                'square_meters' => $faker->numberBetween(20, 200),
+                'lat' => $faker->latitude,
+                'lon' => $faker->longitude,
+                'cover_img' => $faker->imageUrl(640, 480, 'apartments', true),
+                'show' => $faker->boolean,
+            ]);
+        }
     }
 }
