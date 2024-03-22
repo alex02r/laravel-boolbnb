25580e916d88e3db5eb278a1eb1cd7fd8d1f7051
alex02r <alex.giochi02@gmail.com>
2024-03-22 21:24:16 +0100

    upgrade: Aggiunti i controlli per gli errori di ogni input

--- resources/views/user/apartment/create-apartment.blade.php
+++ resources/views/user/apartment/create-apartment.blade.php
@@ -17,7 +17,10 @@
                     {{-- inserimento del titolo --}}
                     <div class="mb-3">
                         <label for="title" class="form-label">Inserisci il titolo:</label>
-                        <input type="text" class="form-control" name="title" id="title" required>
+                        <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}" required>
+                        @error('title')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     
                     {{-- inserimento dell'indirizzo --}}
@@ -27,29 +30,43 @@
 
                     <label for="search">Inserisci l'indirizzo:</label>
                     <div id="search">
-
+                        @error('address')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
                     </div>
                     {{-- inserimento square_meters,rooms, bathrooms, beds --}}
                     <div class="d-flex gap-3 my-3">
                         {{-- square meters --}}
                         <div class="">
                             <label for="square_meters" class="form-label">Metri quadrati</label>
-                            <input type="number" min="10" class="form-control form-control-sm" name="square_meters" id="square_meters" required>                            
+                            <input type="number" min="10" class="form-control form-control-sm" name="square_meters" id="square_meters" value="{{ old('square_meters') }}" required>                            
+                            @error('square_meters')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         {{-- rooms --}}
                         <div class="">
                             <label for="rooms" class="form-label">Stanze</label>
-                            <input type="number" min="0" max="255" class="form-control form-control-sm" name="rooms" id="rooms" required>                            
+                            <input type="number" min="0" max="255" class="form-control form-control-sm" name="rooms" id="rooms" value="{{ old('rooms') }}" required>                            
+                            @error('rooms')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         {{-- bathrooms --}}
                         <div class="">
                             <label for="bathrooms" class="form-label">Bagni</label>
-                            <input type="number" min="0" max="255" class="form-control form-control-sm" name="bathrooms" id="bathrooms" required>                            
+                            <input type="number" min="0" max="255" class="form-control form-control-sm" name="bathrooms" id="bathrooms" value="{{ old('bathrooms') }}" required>                            
+                            @error('bathrooms')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                         {{-- beds --}}
                         <div class="">
                             <label for="beds" class="form-label">Letti</label>
-                            <input type="number" min="0" max="255" class="form-control form-control-sm" name="beds" id="beds" required>                            
+                            <input type="number" min="0" max="255" class="form-control form-control-sm" name="beds" id="beds" value="{{ old('beds') }}" required>                            
+                            @error('beds')
+                                <div class="text-danger">{{ $message }}</div>
+                            @enderror
                         </div>
                     </div>
                     {{-- seleziona visibilità --}}
