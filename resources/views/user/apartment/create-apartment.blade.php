1e0cfb0a26d5907c8a3d4612af82d8d9d2bebd0e
alex02r <alex.giochi02@gmail.com>
2024-03-22 18:29:04 +0100

    upgrade: aggiunto il controllo per gli errori di validazione, aggiunto div container per la searchbox di tom tom e btn submit

--- resources/views/user/apartment/create-apartment.blade.php
+++ resources/views/user/apartment/create-apartment.blade.php
@@ -3,7 +3,16 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-8">
-                <form action="" method="post">
+                @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                <form action="{{ route('user.apartment.store') }}" method="post">
                     @csrf
                     {{-- inserimento del titolo --}}
                     <div class="mb-3">
@@ -12,13 +21,16 @@
                     </div>
                     
                     {{-- inserimento dell'indirizzo --}}
+                    <input type="text" class="d-none" name="address" id="address"> {{-- campo nascosto --}}
+                    <div id="search">
+
+                    </div>
+
                     <div class="mb-3">
-                        <label for="address" class="form-label">Inserisci il indirizzo:</label>
-                        <input type="text" class="form-control" name="address" id="address">
+                        <button type="submit" class="btn btn-success" id="btnAdd">Aggiungi</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
-    
 @endsection
