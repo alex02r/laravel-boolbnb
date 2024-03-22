db43516d3f1cd8a6d5f4bfae58cfb5b76d1c56de
alex02r <alex.giochi02@gmail.com>
2024-03-22 11:00:05 +0100

    upgrade: Aggiunta la struttura per la form di inserimento dati

--- resources/views/user/apartment/create-apartment.blade.php
+++ resources/views/user/apartment/create-apartment.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.layout')
 @section('content')
-    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <form action="" method="post">
+                    @csrf
+                    
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
