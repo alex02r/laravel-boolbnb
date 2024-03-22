8daeb6cdcdb61890c347a707ac4f36f5e4070f6c
alex02r <alex.giochi02@gmail.com>
2024-03-22 20:57:58 +0100

    upgrade: inserimento regole di validazione per i campi del form

--- app/Http/Requests/StoreApartmentRequest.php
+++ app/Http/Requests/StoreApartmentRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Apartment;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreApartmentRequest extends FormRequest
@@ -24,7 +25,17 @@ class StoreApartmentRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'user_id' => 'required', 
+            'title' => 'required|unique:'.Apartment::class, 
+            'rooms' => 'required|min:0|max:255|numeric|integer', 
+            'bathrooms' => 'required|min:0|max:255|numeric|integer', 
+            'beds' => 'required|min:0|max:255|numeric|integer', 
+            'square_meters' => 'required',,
+            'address' => 'required',,
+            'lat' => 'required',,
+            'lon' => 'required',,
+            'cover_img' => 'image|mimes:jpeg,png,jpg,gif,svg',
+            'show',
         ];
     }
 }
