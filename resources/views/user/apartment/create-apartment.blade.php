55ff87df553bbdc37e416e9e9adadca288d58c6e
alex02r <alex.giochi02@gmail.com>
2024-03-22 11:07:10 +0100

    upgrade: Inserimento primo input nella form

--- resources/views/user/apartment/create-apartment.blade.php
+++ resources/views/user/apartment/create-apartment.blade.php
@@ -5,7 +5,11 @@
             <div class="col-8">
                 <form action="" method="post">
                     @csrf
-                    
+                    {{-- inserimento del titolo --}}
+                    <div class="mb-3">
+                        <label for="title" class="form-label">Inserisci il titolo:</label>
+                        <input type="text" class="form-control" name="title" id="title">
+                    </div>
                 </form>
             </div>
         </div>
