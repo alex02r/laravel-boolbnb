afdb905db8e1f71112ab814353d7d33e4cadf6ea
alex02r <alex.giochi02@gmail.com>
2024-03-22 14:05:30 +0100

    upgrade: inserimento della città prima dell'indirizzo

--- resources/views/user/apartment/create-apartment.blade.php
+++ resources/views/user/apartment/create-apartment.blade.php
@@ -10,17 +10,17 @@
                         <label for="title" class="form-label">Inserisci il titolo:</label>
                         <input type="text" class="form-control" name="title" id="title">
                     </div>
+                    {{-- inserimento città e cap--}}
+                    <div class="d-flex mb-3">
+                        <div>
+                            <label for="city" class="form-label">Inserisci la città:</label>
+                            <input type="text" class="form-control" name="city" id="city">
+                        </div>
+                    </div>
                     {{-- inserimento dell'indirizzo --}}
                     <div class="mb-3">
                         <label for="address" class="form-label">Inserisci il indirizzo:</label>
                         <input type="text" class="form-control" name="address" id="address">
-                    </div>
-                    {{-- inserimento città e cap--}}
-                    <div class="d-flex mb-3">
-                        <div>
-                            <label for="address" class="form-label">Inserisci la città:</label>
-                            <input type="text" class="form-control" name="address" id="address">
-                        </div>
                     </div>
                 </form>
             </div>
