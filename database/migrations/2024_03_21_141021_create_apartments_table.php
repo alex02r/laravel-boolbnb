3432984063bad9c4e6d836f4ed16ecabdfbaa054
alex02r <alex.giochi02@gmail.com>
2024-03-22 18:54:04 +0100

    upgrade: aggiunto lo slug

--- database/migrations/2024_03_21_141021_create_apartments_table.php
+++ database/migrations/2024_03_21_141021_create_apartments_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('apartments', function (Blueprint $table) {
             $table->id();
             $table->string('title')->unique();
+            $table->string('slug');
             $table->string('address');
             $table->integer('rooms');
             $table->integer('bathrooms');
