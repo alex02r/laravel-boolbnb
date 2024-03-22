fd66d0b0173ff9d778c82380e596a387eb4ddd86
alex02r <alex.giochi02@gmail.com>
2024-03-22 18:31:44 +0100

    upgrade: script per la creazione della searchbox di tom tom

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,4 +4,29 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+import { services } from '@tomtom-international/web-sdk-services';
+import SearchBox from '@tomtom-international/web-sdk-plugin-searchbox';
+
+//prendiamo il container dove inserire la searchbox
+const div = document.getElementById('search')
+
+//inseriamo l'api in una variabile
+let api = "hr4ctYlqY1McGvola1seXuSFgR5grVBj"
+
+//inizializiamo la searchbox di tomtom
+let options = {
+    searchOptions: {
+    key: api,
+    language: "it-IT",
+    limit: 5,
+    },
+    autocompleteOptions: {
+    key: api,
+    language: "it-IT",
+    },
+}
+var ttSearchBox = new tt.plugins.SearchBox(tt.services, options);
+var searchBoxHTML = ttSearchBox.getSearchBoxHTML();
+//facciamo l'append della searchbox al div container
+div.append(searchBoxHTML)
 
