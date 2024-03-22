e0a32e7377fbd63c5c6ab5fcb28d2717a85b33f2
alex02r <alex.giochi02@gmail.com>
2024-03-22 12:25:49 +0100

    upgrade: eseguita chiamata api per ogni lettera che inseriamo nel campo address, per avere l'autocomplete

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,20 @@
 import './bootstrap';
 import '~resources/scss/app.scss';
 import * as bootstrap from 'bootstrap';
+import axios from 'axios';
 import.meta.glob([
     '../img/**'
 ])
 
+//prendiamo tramite id il campo input dove inseriamo l'address
 const address = document.getElementById('address')
-
+//inseriamo il contenuto in una variabile
+let query = address.value;
+//controlliamo quando viene inserita una lettera nel campo address
 address.addEventListener("keyup", function(){
-    alert('ciao');
+    alert(query);
+    //eseguiamo una chiamata API per avere un autocomplete
+    axios.get(`${process.env.TOMTOM_BASE_URL}//search/2/autocomplete/${query}.json?key=${process.env.TOMTOM_API_KEY}&language=it-IT`).then(response =>{
+        
+    })
 })
