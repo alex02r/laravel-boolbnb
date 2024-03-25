552965c98dc0540154f6cda5182b6bb3fe83119c
alex02r <alex.giochi02@gmail.com>
2024-03-25 17:08:42 +0100

    fix: sisteamta la card dello show

--- resources/views/user/apartment/show.blade.php
+++ resources/views/user/apartment/show.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.layout')
 
 @section('content')
-    <div class="card color_card my-3" style="width: 75vw;">
-
-        <div>
-            <img class="w-100"
-                src="{{ $apartment->cover_img !== 'null' ? asset('/storage/' . $apartment->cover_img) : '/storage/img/imgnull.jpg' }}"
-                alt="">
+<div class="container">
+    <div class="row justify-content-center my-3">
+        <div class="col-8">
+            <div class="card color_card py-3">
+                
+                <img class="card-img-top w-75 align-self-center" src="{{ $apartment->cover_img !== 'null' ? asset('/storage/' . $apartment->cover_img) : '/storage/img/imgnull.jpg' }}" alt="{{ $apartment->title }}">
+                <div class="card-body">
+                    <h2 class="card-title">{{ $apartment->title }}</h2>
+                    <h5 class="text-secondary">{{ $apartment->address }}</h5>
+                    <p class="card-text">
+                        {{ $apartment->rooms }} {{ $apartment->rooms == 1 ? ' Camera' : ' Camere' }}
+                        
+                        - {{ $apartment->bathrooms }} {{ $apartment->rooms == 1 ? ' Bagno' : ' Bagni' }}
+                        
+                        - {{ $apartment->beds }} {{ $apartment->beds == 1 ? ' Letto' : ' Letti' }}
+                        
+                        - {{ $apartment->square_meters }} Mq
+                    </p>
+                </div>
+            </div>
         </div>
-        <h1 class="card-title m-2">{{ $apartment['title'] }}</h5>
-            <h2 class="p-2">{{ $apartment['address'] }}</h2>
-            <h4 class="p-2">
-                {{ $apartment['rooms'] }} {{ $apartment['rooms'] == 1 ? ' Camera' : ' Camere' }}
-                &nbsp;
-                {{ $apartment['bathrooms'] }} {{ $apartment['rooms'] == 1 ? ' Bagno' : ' Bagni' }}
-                &nbsp;
-                {{ $apartment['beds'] }} {{ $apartment['beds'] == 1 ? ' Letto' : ' Letti' }}
-                &nbsp;
-                {{ $apartment['square_meters'] }} Mq
-            </h4>
     </div>
+</div>
 @endsection
