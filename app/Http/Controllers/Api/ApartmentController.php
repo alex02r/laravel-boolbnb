cea65bfbe67345679e90e3d3bbac1f25caeb8737
DorinVieru <dorino.vieru@gmail.com>
2024-03-26 16:13:32 +0100

    up: creazione rotta api per apartment

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -4,8 +4,19 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Apartment;
 
 class ApartmentController extends Controller
 {
-    //
+    public function index()
+    {
+        $apartments = Apartment::all();
+        
+        return response()->json([
+            'success' => true,
+            'results' => $apartments
+        ]);
+    }
 }
