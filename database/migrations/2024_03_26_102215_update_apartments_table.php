bf61cddddf77c4a36497c066e5e7a1857e27ca5b
alex02r <alex.giochi02@gmail.com>
2024-03-26 12:09:36 +0100

    fix: aggiunto il dropunique nella migration

--- database/migrations/2024_03_26_102215_update_apartments_table.php
+++ database/migrations/2024_03_26_102215_update_apartments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('apartments', function (Blueprint $table) {
-            $table->string('title')->change();
+            $table->dropUnique(['title'])->change();
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('apartments', function (Blueprint $table) {
-            
+            $table->unique(['title'])->change();
         });
     }
 };
