4a6ae4d5c734402768201aee1e5852fcc11387ce
alex02r <alex.giochi02@gmail.com>
2024-03-26 19:36:12 +0100

    upgadr: aggiunto commento per controllo degli indirizzi vicini

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -20,12 +20,27 @@ class ApartmentController extends Controller
         ]);
     }
     
-    public function search($value){
-        $apartments = Apartment::where('address', $value);
-        //invio dei dati come risposa alla chiamata API
-        return response()->json([
-            'success' => true,
-            'apartments' => $apartments
-        ]);
+    public function search($address){
+        $apartments = Apartment::where('address', 'LIKE', $address)->paginate(6);
+        //fare una query, dove facciamo la select di lat e lon del primo risultato where address = $address
+
+        //chimata API a tom tom nearby con lat e lon trovate prima
+            //foreach di freeformAddress del risultato
+            //assegno a una variabile apartments[]= where address == freeformAddress
+        
+
+        if (!empty($apartments)) {
+            //invio dei dati come risposa alla chiamata API
+            return response()->json([
+                'success' => true,
+                'apartments' => $apartments
+            ]);
+        }else{
+            return response()->json([
+                'success' => false,
+                'error' => 'non sono stati trovati apartments con questo indirizzo'
+            ]);
+        }
+        
     }
 }
