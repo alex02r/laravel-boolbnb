be420e5e1022a06462828f7ace805cb639ca552f
alex02r <alex.giochi02@gmail.com>
2024-03-27 18:33:03 +0100

    fix: cambiato il nome della rotta per la ricerca degli apartments

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\Api\ApartmentController;
 
 
 Route::get('/apartment', [ApartmentController::class, 'index']);
-Route::get('/apartments/{address}', [ApartmentController::class, 'search']);
+Route::get('/search', [ApartmentController::class, 'search']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
