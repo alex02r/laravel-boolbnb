7b03b9fe1121145573ba2a350d1b58ff61b0037b
DorinVieru <dorino.vieru@gmail.com>
2024-03-28 13:50:40 +0100

    up: aggiunte due pagine di errore, not found e not authorized

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404 - Not Found</title>
+
+    <style>
+    .content{
+    margin-top: 200px;
+
+        h1{
+            font-size: 80px;
+            color: #ff5555;
+        }
+
+        p{
+            font-size: 25px;
+            margin: 30px 0px;
+        }
+    }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center ">
+                <div class="content">
+                    <h1>404</h1>
+                    <p>Oops! Sembra che tu stia cercando qualcosa che non è presente all'interno della piattaforma.</p>
+                    <p>Ritorna alla tua <a href="{{ route('user.dashboard') }}"></a>dashboard personale</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
