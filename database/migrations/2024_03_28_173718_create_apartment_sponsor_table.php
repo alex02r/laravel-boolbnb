b90be077c06afa77d2fafb449710a93699908b61
alex02r <alex.giochi02@gmail.com>
2024-03-28 18:40:13 +0100

    upgrade: assegnazione delle FK

--- database/migrations/2024_03_28_173718_create_apartment_sponsor_table.php
+++ database/migrations/2024_03_28_173718_create_apartment_sponsor_table.php
@@ -14,8 +14,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('apartment_sponsor', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->unsignedBigInteger('apartment_id');
+            $table->foreign('apartment_id')->references('id')->on('apartments')->cascadeOnDelete('set null');
+
+            $table->unsignedBigInteger('sponsor_id');
+            $table->foreign('sponsor_id')->references('id')->on('sponsors')->cascadeOnDelete('set null');
+            
+            $table->date('ending_date');
         });
     }
 
