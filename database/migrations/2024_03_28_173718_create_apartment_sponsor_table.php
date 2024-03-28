48721b61ef45952233184d720e1a3b7811b27f24
alex02r <alex.giochi02@gmail.com>
2024-03-28 19:31:16 +0100

    fix: aggiunto valore starting_date

--- database/migrations/2024_03_28_173718_create_apartment_sponsor_table.php
+++ database/migrations/2024_03_28_173718_create_apartment_sponsor_table.php
@@ -20,8 +20,8 @@ return new class extends Migration
             $table->unsignedBigInteger('sponsor_id');
             $table->foreign('sponsor_id')->references('id')->on('sponsors')->cascadeOnDelete('set null');
             
+            $table->date('start_date');
             $table->date('ending_date');
-            $table->timestamps();
         });
     }
 
