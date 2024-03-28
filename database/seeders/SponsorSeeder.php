a5437224a989bd45edcba21bafb7751de9619556
alex02r <alex.giochi02@gmail.com>
2024-03-28 18:32:53 +0100

    upgrade: foreach sull'array per la creazione degli sponsor

--- database/seeders/SponsorSeeder.php
+++ database/seeders/SponsorSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Sponsor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,19 +18,27 @@ class SponsorSeeder extends Seeder
         $sponsors = [
             [
                 'title' => 'Base',
-                'duration' => 0,
+                'duration' => 24,
                 'price' => 2.99 
             ],
             [
                 'title' => 'Media',
-                'duration' => 0,
+                'duration' => 72,
                 'price' => 5.99 
             ],
             [
-                'title' => 'Pro',
-                'duration' => 0,
+                'title' => 'Avanzata',
+                'duration' => 144,
                 'price' => 9.99 
             ]
         ];
+
+        foreach ($sponsors as $sponsor) {
+            $new_sponsor = new Sponsor();
+            $new_sponsor->title = $sponsor['title'];
+            $new_sponsor->duration = $sponsor['duration'];
+            $new_sponsor->price = $sponsor['price'];
+            $new_sponsor->save();
+        }
     }
 }
