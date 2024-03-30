53ef6c623940e9959823dc7785e0c7772dc12372
alex02r <alex.giochi02@gmail.com>
2024-03-30 12:28:25 +0100

    upgrade: regole di validazione

--- app/Http/Requests/StoreSponsorApartmentRequest.php
+++ app/Http/Requests/StoreSponsorApartmentRequest.php
@@ -24,13 +24,18 @@ class StoreSponsorApartmentRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'start_date' => 'required|date',
+            'start_time' => 'required|date_format:H:i'
         ];
     }
     public function messages()
     {
         return [
-            //
+            'start_date.required' => 'la data deve essere obbligatorio',
+            'start_date.date' => 'Devi inserire una data',
+
+            'start_time.required' => 'l\'orario deve essere obbligatorio',
+            'start_date.date_format' => 'Non hai inserito un formato di ora adatto'
         ];
     }
 }
