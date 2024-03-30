1a19c03f140e08e02e180b85090af4074a4c38a2
alex02r <alex.giochi02@gmail.com>
2024-03-30 14:17:31 +0100

    upgrade: visualizzazioni degli errori di validazione e aggiunti name e id dei campi

--- resources/views/user/sponsor/sponsor-apartment.blade.php
+++ resources/views/user/sponsor/sponsor-apartment.blade.php
@@ -7,19 +7,28 @@
                 <h2>{{ $apartment->title }}</h2>
             </div>
             <div class="col-12 col-md-8">
-                <form action="" method="POST">
+                @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <form action="{{ route('user.payment', ['apartment' => $apartment, 'sponsor' => $sponsor]) }}" method="post">
                     @csrf
                     <div class="row">
                         <div class="col-12 col-md-4">
                             <div class="mb-3">
                                 <label for="exampleFormControlInput1" class="form-label">Seleziona la data di inizio</label>
-                                <input type="date" class="form-control" min="{{ date("Y-m-d") }}" name="" id="">
+                                <input type="date" class="form-control" min="{{ date("Y-m-d") }}" name="start_date" id="start_date" value="{{ old('start_date') }}" required>
                             </div>
                         </div>
                         <div class="col-12 col-md-4">
                             <div class="mb-3">
                                 <label for="exampleFormControlInput1" class="form-label">Inserisci l'orario di inizio</label>
-                                <input type="time" class="form-control" name="" id="">
+                                <input type="time" class="form-control" name="start_time" id="start_time" value="{{ old('start_time') }}" required>
                             </div>
                         </div>
                         <div class="col-12">
