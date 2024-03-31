980ec9437b20b23ccec1bd27c04fd7ce06bb20d0
DorinVieru <dorino.vieru@gmail.com>
2024-03-31 12:48:38 +0200

    up: cambiato il design della pagine welcome in cui veniamo reindirizzati dopo aver effettuato il logout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,35 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="jumbotron p-5 mb-4 bg-light rounded-3">
+<div class="jumbotron p-3 bg-light rounded-3">
     <div class="container py-5">
         <div class="logo_laravel text-danger fs-1 fw-bold">
             BoolBnB
         </div>
-        <h1 class="display-5 fw-bold">
+        <h1 class="display-5 mb-5">
             Benvenuto/a nella tua personale Dashboard di BoolBnB
         </h1>
-
-        <p class="col-md-8 fs-4">Questo è un preset creato da BoolBnB con Bootstrap 5 per progetti Laravel e include laravel/blade. Lavora dalla versione 9x di Laravel fino all'ultima attualmente disponibile, la 11x. Se vuoi avere altre informazioni clicca sul bottone per visionare la documentazione.</p>
-        <a href="https://packagist.org/packages/pacificdev/laravel_9_preset" class="btn btn-primary btn-lg" type="button">Documentazione</a>
-    </div>
-</div>
-
-<div class="content">
-    <div class="container">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <h5 class="card-header bg-dark text-center text-white"> <i class="fas fa-user"></i> Sei un utente registrato?</h5>
+                    <div class="card-body text-center">
+                        <p class="card-text">Effettua il login.</p>
+                        <button class="btn btn-primary"><a class="text-decoration-none text-white" href="http://127.0.0.1:8000/login">Accedi</a></button>
+                    </div>
+                    <div class="card-footer fst-italic text-center">
+                        OPPURE
+                    </div>
+                    <div class="card-body text-center">
+                        <p class="card-text">Puoi registrarti ed entrare a far parte della nostra piattaforma!</p>
+                        <button class="btn btn-primary"><a class="text-decoration-none text-white" href="http://127.0.0.1:8000/register">Registrati</a></button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 text-center display-7 mt-5">
+                <p>Vuoi tornare alla Homepage del nostro sito? <a class="link-info" href="http://localhost:5174">Clicca qui.</a></p>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
