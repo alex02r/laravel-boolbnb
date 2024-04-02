0fcf442faa6ad3a561e0bf6931db8a2c5160e578
alex02r <alex.giochi02@gmail.com>
2024-04-02 18:50:43 +0200

    upgrade: script per gestire il pagamento

--- resources/views/user/sponsor/sponsor-apartment.blade.php
+++ resources/views/user/sponsor/sponsor-apartment.blade.php
@@ -73,4 +73,31 @@
             </div>
         </div>
     </div>
+    <script type="text/javascript">
+        //braintree 
+        const form = document.getElementById('payment-form');
+
+        braintree.dropin.create({
+            authorization: 'CLIENT_AUTHORIZATION',
+            container: '#dropin-container'
+        }, (error, dropinInstance) => {
+            if (error) console.error(error);
+
+            form.addEventListener('submit', event => {
+                event.preventDefault();
+
+                dropinInstance.requestPaymentMethod((error, payload) => {
+                if (error) console.error(error);
+
+                // Step four: when the user is ready to complete their
+                //   transaction, use the dropinInstance to get a payment
+                //   method nonce for the user's selected payment method, then add
+                //   it a the hidden field before submitting the complete form to
+                //   a server-side integration
+                document.getElementById('nonce').value = payload.nonce;
+                form.submit();
+                });
+            });
+        });
+    </script>
 @endsection
