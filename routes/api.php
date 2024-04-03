16ace3cdec70095551d18d69031f42f11c8903be
DorinVieru <dorino.vieru@gmail.com>
2024-04-03 12:43:01 +0200

    up: creazione del controller per la view e rotta api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ApartmentController;
 use App\Http\Controllers\Api\MessageController;
+use App\Http\Controllers\Api\ViewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +23,7 @@ Route::get('/search', [ApartmentController::class, 'search']);
 Route::get('/single/apartment/{slug}/{id}', [ApartmentController::class, 'singleApartment']);
 
 Route::post('/message', [MessageController::class, 'store']);
+Route::post('/view', [MessageController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
