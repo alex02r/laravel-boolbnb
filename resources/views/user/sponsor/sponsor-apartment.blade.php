d6e83ebdbb03dc624057913c28287b51f251add3
alex02r <alex.giochi02@gmail.com>
2024-04-03 15:59:34 +0200

    upgrade: aggiunto stile alla form

--- resources/views/user/sponsor/sponsor-apartment.blade.php
+++ resources/views/user/sponsor/sponsor-apartment.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.layout')
 @section('content')
     <div class="container">
-        <div class="row">
+        <div class="row bg-white rounded p-2">
             <div class="col-12">
-                <h1>Sponsorizzaione :</h1>
-                <h2>{{ $apartment->title }}</h2>
+                <h2>Sponsorizzaione: {{ $apartment->title }}</h2>
             </div>
             <div class="col-12 col-md-8">
                 @if ($errors->any())
@@ -17,24 +16,26 @@
                     </div>
                 @endif
                 
-                
-                <form action="{{ route('user.payment', ['apartment' => $apartment, 'sponsor' => $sponsor]) }}" method="post" id="payment-form" class="my-5">
+                <form action="{{ route('user.payment', ['apartment' => $apartment, 'sponsor' => $sponsor]) }}" method="post" id="payment-form" class="my-3">
                     @csrf
                     <div class="row">
                         <div class="col-12 col-md-4">
                             <div class="mb-3">
-                                <label for="start_date" class="form-label">Seleziona la data di inizio</label>
+                                <label for="start_date" class="form-label">Seleziona la data d'inizio</label>
                                 <input type="date" class="form-control" min="{{ date("Y-m-d") }}" name="start_date" id="start_date" value="{{ old('start_date') }}" required>
                             </div>
                         </div>
                         <div class="col-12 col-md-4">
                             <div class="mb-3">
-                                <label for="end_date" class="form-label">Inserisci l'orario di inizio</label>
+                                <label for="end_date" class="form-label">Inserisci l'orario d'inizio</label>
                                 <input type="time" class="form-control" name="start_time" id="start_time" value="{{ old('start_time') }}" required>
                             </div>
                         </div>
-                        <div class="col-12">
-                            <h5>Prezzo: {{ $sponsor->price }}</h5>
+                        <div class="col-12 col-md-8">
+                            <h5> Sponsor {{ $sponsor->title }}</h5>
+                            <h6>Durata: {{ $sponsor->duration }}h</h6>
+                            <h6>Prezzo: {{ $sponsor->price }}€</h6>
+
                             <div id="dropin-container"></div>
                             <input id="nonce" name="payment_method_nonce" type="hidden" />
                             <input type="hidden" name="token" id="token" value="{{ $token }}" />
@@ -46,6 +47,8 @@
                     </div>
                 </form>
             </div>
+        </div>
+        <div class="row">
             <div class="col-12 mt-3">
                 <h2> Sponsorizzaioni già presenti:</h2>
                 @if (count($apartment->sponsors) > 0)
