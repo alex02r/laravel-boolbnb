9b7a5d768e12cd955857a32cd8f135aa1f90bd81
DorinVieru <dorino.vieru@gmail.com>
2024-04-04 17:18:38 +0200

    up: creazione tabella per id messaggi

---
+++ database/migrations/2024_04_04_143948_add_message_id_to_views_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('views', function (Blueprint $table) {
+            $table->unsignedBigInteger('message_id')->nullable()->after('apartment_id');
+
+            $table->foreign('message_id')->references('id')->on('messages')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('views', function (Blueprint $table) {
+            $table->dropForeign('views_message_id_foreign');
+
+            $table->dropColumn('message_id');
+        });
+    }
+};
